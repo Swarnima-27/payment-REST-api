5d318c4dddc4b30a9408286db531e4bacd018e1f
Swarnima-27 <lit2017004@iiitl.ac.in>
2021-05-22 21:56:05 +0530

    implementation classes  created

--- Onboarding/src/main/java/com/basic/paymentapp/OnboardService/ServiceImpl/BankServiceImpl.java
+++ Onboarding/src/main/java/com/basic/paymentapp/OnboardService/ServiceImpl/BankServiceImpl.java
@@ -1,4 +1,52 @@
 package com.basic.paymentapp.OnboardService.ServiceImpl;
 
-public class BankServiceImpl {
+import com.basic.paymentapp.OnboardService.ServiceInterface.BankServiceInt;
+import com.basic.paymentapp.entities.PartnerBanks;
+import com.basic.paymentapp.entities.Response;
+import com.basic.paymentapp.exceptions.AlreadyExistsException;
+import com.basic.paymentapp.repositories.PartnerBankRepo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class BankServiceImpl implements BankServiceInt {
+    private static final Logger log= LoggerFactory.getLogger(BankServiceImpl.class);
+
+    @Autowired
+    PartnerBankRepo partnerBankRepo;
+
+    @Override
+    public boolean addbank(PartnerBanks partnerBanks) {
+        if(partnerBankRepo.existsById(partnerBanks.getBankid()))
+        {
+            log.info(partnerBanks.getBankid()+" already exists");
+            throw new AlreadyExistsException(partnerBanks.getBankid()+" already exists");
+        }
+        else
+        {
+            partnerBankRepo.save(partnerBanks);
+            log.info("Bank saved");
+            return true;
+        }
+    }
+
+    @Override
+    public List<Response> getAllbankdata() {
+        List<Response> banklist= new ArrayList<>();
+        partnerBankRepo.findAll().forEach(entity->{
+            try{
+                banklist.add(new Response("success",entity,"No error"));
+            }
+            catch(Exception exception)
+            {
+                banklist.add(new Response("Error in loading",null,exception.getMessage()));
+            }
+        });
+        return banklist;
+    }
 }
--- Onboarding/src/main/java/com/basic/paymentapp/OnboardService/ServiceImpl/UserServiceImpl.java
+++ Onboarding/src/main/java/com/basic/paymentapp/OnboardService/ServiceImpl/UserServiceImpl.java
@@ -1,4 +1,93 @@
 package com.basic.paymentapp.OnboardService.ServiceImpl;
 
-public class UserServiceImpl {
+import com.basic.paymentapp.OnboardService.ServiceInterface.UserServiceInt;
+import com.basic.paymentapp.entities.Response;
+import com.basic.paymentapp.entities.Users;
+import com.basic.paymentapp.exceptions.AlreadyExistsException;
+import com.basic.paymentapp.exceptions.EmptyInputException;
+import com.basic.paymentapp.exceptions.NotFoundException;
+import com.basic.paymentapp.repositories.UsersRepo;
+import com.basic.paymentapp.repositories.WalletRepo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class UserServiceImpl implements UserServiceInt {
+
+    private static final Logger log= LoggerFactory.getLogger(UserServiceImpl.class);
+
+    @Autowired
+    UsersRepo usersRepo;
+
+    @Autowired
+    WalletRepo walletRepo;
+
+    @Override
+    public List<Response> getAlldata() {
+        List<Response> userlist= new ArrayList<>();
+        usersRepo.findAll().forEach(entity->{
+            try{
+                userlist.add(new Response("success",entity,"No error"));
+            }
+            catch(Exception exception)
+            {
+                userlist.add(new Response("Error in loading",null,exception.getMessage()));
+            }
+        });
+        return userlist;
+
+    }
+
+    @Override
+    public boolean addUser(Users user) {
+        if(user.getEmail().isEmpty() || user.getFirstname().isEmpty() || user.getPhone_number().isEmpty())
+            throw new EmptyInputException("Values cannot be null");
+        else if(usersRepo.findById(user.getPhone_number()).isPresent())
+        {
+            log.info("User already exists with "+ user.getPhone_number()+" phone number");
+            throw new AlreadyExistsException("User already exists with "+ user.getPhone_number()+" phone number");
+        }
+        else
+        {
+            usersRepo.save(user);
+            log.info("User Created");
+            return true;
+        }
+    }
+
+    @Override
+    public boolean updateuser(Users user) {
+        if(user.getEmail().isEmpty() || user.getFirstname().isEmpty() || user.getPhone_number().isEmpty())
+            throw new EmptyInputException("Values cannot be null");
+        else if(usersRepo.findById(user.getPhone_number()).isPresent())
+        {
+            usersRepo.save(user);
+            log.info("User Updated");
+            return true;
+        }
+        else
+        {
+            log.info("User does not exists");
+            throw new NotFoundException("User already exists with "+ user.getPhone_number()+" phone number");
+        }
+    }
+
+    @Override
+    public Users getUserbyId(String Phoneno) {
+        Optional<Users> user1=usersRepo.findById(Phoneno);
+        if(user1.isPresent())
+        {
+            log.info("User info available " + user1.get());
+            return user1.get();
+        }
+        else
+        {
+            log.info("User with " + Phoneno + " does not exists");
+            throw new NotFoundException("User with " + Phoneno + " does not exists");
+        }
+    }
 }
