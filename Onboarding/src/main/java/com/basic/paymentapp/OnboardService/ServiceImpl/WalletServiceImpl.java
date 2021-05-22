83d22a5d036716184164e44968eb8d150e6860b7
Swarnima-27 <lit2017004@iiitl.ac.in>
2021-05-22 22:00:10 +0530

    implementation classes  created

--- Onboarding/src/main/java/com/basic/paymentapp/OnboardService/ServiceImpl/WalletServiceImpl.java
+++ Onboarding/src/main/java/com/basic/paymentapp/OnboardService/ServiceImpl/WalletServiceImpl.java
@@ -1,4 +1,34 @@
 package com.basic.paymentapp.OnboardService.ServiceImpl;
 
-public class WalletServiceImpl {
+import com.basic.paymentapp.OnboardService.ServiceInterface.WalletServiceInt;
+import com.basic.paymentapp.entities.Wallet;
+import com.basic.paymentapp.exceptions.NotFoundException;
+import com.basic.paymentapp.repositories.WalletRepo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class WalletServiceImpl implements WalletServiceInt {
+
+    private static final Logger log= LoggerFactory.getLogger(WalletServiceImpl.class);
+    @Autowired
+    WalletRepo walletRepo;
+
+    @Override
+    public boolean check(String phoneno) {
+        return walletRepo.findById(phoneno).isPresent();
+    }
+
+    @Override
+    public void save(Wallet wallet) {
+        walletRepo.save(wallet);
+    }
+
+    @Override
+    public Wallet getwallet(String phoneno) {
+        if(walletRepo.findById(phoneno).isPresent())
+            return walletRepo.findById(phoneno).get();
+        else
+            throw new NotFoundException("WalletID " + phoneno + " not found");
+    }
 }
