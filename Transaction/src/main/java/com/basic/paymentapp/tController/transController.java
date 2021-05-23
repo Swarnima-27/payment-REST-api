edaee45e4d37ff645b5ad9e908f31dc19408024f
Swarnima-27 <lit2017004@iiitl.ac.in>
2021-05-23 17:14:25 +0530

    Transaction controller created

---
+++ Transaction/src/main/java/com/basic/paymentapp/tController/transController.java
@@ -0,0 +1,82 @@
+package com.basic.paymentapp.tController;
+
+import com.basic.paymentapp.TransactionService.TransServiceImpl;
+import com.basic.paymentapp.entities.Transactions;
+import com.basic.paymentapp.exceptions.NotFoundException;
+import com.basic.paymentapp.exceptions.NotValidException;
+import com.basic.paymentapp.exceptions.TransactionFailedException;
+import com.basic.paymentapp.repositories.WalletRepo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value="/transaction")
+public class transController {
+    private static final Logger log= LoggerFactory.getLogger(transController.class);
+    @Autowired
+    WalletRepo walletRepo;
+
+    @Autowired
+    TransServiceImpl transService;
+
+    @PostMapping(value="/transfer")
+    public ResponseEntity<String> transferMoney(@RequestParam("walletid1") String to_walletid,
+                                                @RequestParam("walletid2") String from_walletid,
+                                                @RequestParam("amount") Double amount)
+    {
+        if(amount<=0)
+            throw new NotValidException("Amount Not Valid");
+        boolean result=transService.transfermoney(to_walletid,from_walletid,amount);
+        if(result)
+            return new ResponseEntity<>("Money transferred", HttpStatus.OK);
+        else
+            throw new TransactionFailedException("Transaction Failed.Something went wrong");
+    }
+
+    @GetMapping(value="/balance")
+    public ResponseEntity<String> checkbalance(@RequestParam("walletid") String walletid)
+    {
+        if(walletRepo.existsById(walletid))
+        {
+            Double balance= transService.checkbalance(walletid);
+            return new ResponseEntity<>("Balance : "+ balance,HttpStatus.OK);
+        }
+        else
+        {
+            log.info("WAllet doesn't exists");
+            throw new NotFoundException("Wallet with ID : " + walletid+" doesn't exists");
+        }
+    }
+
+    @PostMapping(value="/addmoney")
+    public ResponseEntity<String> addmoney(@RequestParam("walletid") String walletid,
+                                           @RequestParam("amount") Double amount)
+    {
+        if(amount<=0)
+            throw new NotValidException("Amount Not Valid");
+        boolean result=transService.addmoney(walletid,amount);
+        if(result)
+            return new ResponseEntity<>("Money added to wallet :" + walletid, HttpStatus.OK);
+        else
+            throw new TransactionFailedException("Transaction Failed.Something went wrong");
+    }
+
+    @GetMapping(value = "/transferby")
+    public ResponseEntity<List<Transactions>> findbyPayerid(@RequestParam("walletid") String walletid)
+    {
+        List<Transactions> translist=transService.findbyPayerid(walletid);
+        if(translist.size()<=0)
+        {
+            log.warn("No transaction or user does not exists");
+            throw new NotFoundException("No transaction or user does not exists");
+        }
+        else
+            return new ResponseEntity<>(translist,HttpStatus.OK);
+    }
+}
